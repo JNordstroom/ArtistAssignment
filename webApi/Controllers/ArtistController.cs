465b5fadc031b188b10ede1755516dfaa7678d50
Jimmie <113166956+JNordstroom@users.noreply.github.com>
2023-11-22 17:38:24 +0100

    Created Put endpoint to update an existing artist

--- webApi/Controllers/ArtistController.cs
+++ webApi/Controllers/ArtistController.cs
@@ -60,5 +60,29 @@ namespace webApi.Controllers
 
             return Ok("Artist tillagd!");
         }
+
+        [HttpPut("{artistId}")]
+        public async Task<IActionResult> UpdateArtist(int artistId, Artister updateArtist)
+        {
+            if(updateArtist == null || artistId <= 0)
+            {
+                return BadRequest("Felaktig inmatning!");
+            }
+
+            var existingArtist = await _context.Artister.FindAsync(artistId);
+
+            if(existingArtist == null)
+            {
+                return BadRequest("Artisten finns inte!");
+            }
+
+            existingArtist.Namn = updateArtist.Namn;
+            existingArtist.Beskrivning = updateArtist.Beskrivning;
+
+            await _context.SaveChangesAsync();
+
+            return Ok("Artisten är uppdaterad!");
+        }
+
     }
 }
