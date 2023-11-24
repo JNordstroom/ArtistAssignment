e639ae9cee5098c022a9b032a82e51a170f37b44
Jimmie <113166956+JNordstroom@users.noreply.github.com>
2023-11-24 19:24:52 +0100

    updated program.cs,artistcontroller,app,artistview,vite.config

--- webReact/src/views/ArtistView.jsx
+++ webReact/src/views/ArtistView.jsx
@@ -1,3 +1,32 @@
+import { useState, useEffect } from 'react';
+import { get } from '../utilsAndHooks/ApiService.jsx'; // Make sure to import your get function
+
 export default function ArtistView() {
-    return <h1>I am the artist page!</h1>;
-  }
+  const [artists, setArtists] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const artistData = await get('Artist/alla');
+        console.log(artistData);
+        setArtists(artistData);
+      } catch (error) {
+        console.error('Error fetching artist data:', error);
+      }
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>Artists</h1>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>
+            <strong>{artist.namn}</strong>: {artist.beskrivning}
+          </li>
+          
+        ))}
+      </ul>
+    </div>
+  );
+}
--- webReact/vite.config.js
+++ webReact/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5272',
+        target: 'http://localhost:5257',
         changeOrigin: true,
         secure: false
       }
