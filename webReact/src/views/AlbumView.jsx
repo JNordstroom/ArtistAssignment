6b328f610347893642a2e4d62d59685dc93be072
Jimmie <113166956+JNordstroom@users.noreply.github.com>
2023-11-27 16:04:51 +0100

    added artist name on album page

--- webReact/src/views/AlbumView.jsx
+++ webReact/src/views/AlbumView.jsx
@@ -5,6 +5,7 @@ import { Container, Row, Col, Card } from 'react-bootstrap';
 
 export default function AlbumView() {
   const { id } = useParams();
+  const [artist, setArtist] = useState({});
   const [albums, setAlbums] = useState([]);
 
   useEffect(() => {
@@ -13,6 +14,7 @@ export default function AlbumView() {
         const albumData = await get(`Album/artist/${id}`);
         console.log('API Response:', albumData);
         setAlbums(albumData.album);
+        setArtist(albumData); // Set the artist state with the API response
       } catch (error) {
         console.error('Error fetching album data:', error);
       }
@@ -23,14 +25,16 @@ export default function AlbumView() {
 
   return (
     <Container className="mt-4">
-      <h1 className="text-center mb-4">Albums</h1>
+      <h1 className="text-center mb-2">Albums of</h1>
+      <h4 className="text-center mb-4">{artist.artistNamn}</h4>
       <Row xs={1} md={2} lg={4} className="g-4 justify-content-center">
         {albums.map((album) => (
           <Col key={album.id} className="d-flex">
             <Card style={{ width: '18rem' }} className="mx-auto">
               <Card.Body className="d-flex flex-column align-items-center justify-content-center">
                 <Card.Title>{album.namn}</Card.Title>
-                <Card.Subtitle className="mb-2 text-muted">{album.placering}</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">Publicerad</Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: '0.8rem' }}>{album.publicerad}</Card.Subtitle>
               </Card.Body>
             </Card>
           </Col>
