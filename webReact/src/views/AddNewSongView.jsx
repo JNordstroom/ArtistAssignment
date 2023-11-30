f1fe443350487eec0a61cb65d4632a7162388b41
Jimmie <113166956+JNordstroom@users.noreply.github.com>
2023-11-30 17:23:12 +0100

    Updated my form to a react bootstrap form

--- webReact/src/views/AddNewSongView.jsx
+++ webReact/src/views/AddNewSongView.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import { post } from '../utilsAndHooks/ApiService';
-import { useNavigate, useParams  } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
 
 const CreateNewAlbumView = () => {
-  const [formData, setFormData] = useState({ Namn: '', Placering: null, AlbumId: null});
+  const [formData, setFormData] = useState({ Namn: '', Placering: null, AlbumId: null });
   const navigate = useNavigate();
-  const { id: albumId } = useParams(); 
+  const { id: albumId } = useParams();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,26 +18,24 @@ const CreateNewAlbumView = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     console.log(albumId);
     console.log(formData);
     try {
-        
       const response = await post('låt/addnewsong', {
         ...formData,
         Namn: formData.Namn,
         // Parsing the user input
-        Placering: parseInt(formData.Placering, 10),  
+        Placering: parseInt(formData.Placering, 10),
         AlbumId: parseInt(albumId, 10),
       });
-  
+
       if (response.error) {
         console.error('Fel vid försök att lägga till låten:', response.error);
       } else {
         alert('Låten tillagd!');
-        
         // route back to that the songpage
-        navigate(`/Songs/${albumId}`);  
+        navigate(`/Songs/${albumId}`);
       }
     } catch (error) {
       console.error('Fel vid försök att skapa låt:', error);
@@ -50,43 +49,36 @@ const CreateNewAlbumView = () => {
   return (
     <div>
       <h1>Lägg till ny låt</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-          <label htmlFor="Namn" className="form-label">
-            Namn
-          </label>
-          <input
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="Namn">
+          <Form.Label>Namn</Form.Label>
+          <Form.Control
             type="text"
-            className="form-control"
-            id="Namn"
             name="Namn"
             value={formData.Namn}
             onChange={handleChange}
             required
           />
-        </div>
+        </Form.Group>
 
-        
-        <div className="mb-3">
-          <label htmlFor="Placering" className="form-label">
-            Placering
-          </label>
-          <textarea
-            className="form-control"
-            id="Placering"
+        <Form.Group className="mb-3" controlId="Placering">
+          <Form.Label>Placering</Form.Label>
+          <Form.Control
+            as="textarea"
             name="Placering"
             value={formData.Placering}
             onChange={handleChange}
             required
-          ></textarea>
-        </div>
+          />
+        </Form.Group>
 
-        
-        <button type="submit" className="btn btn-primary">
-          Lätt till låten
-        </button>
-        <button onClick={goBack}>Tillbaka</button>
-      </form>
+        <Button type="submit" variant="primary" style={{ marginRight: '0.5rem' }}>
+          Lägg till låten
+        </Button>
+        <Button variant="secondary" onClick={goBack} className="mr-x">
+          Tillbaka
+        </Button>
+      </Form>
     </div>
   );
 };
